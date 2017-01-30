777efbaacbe845cb427523e6403c70fcd1c11711
Jacks0N23 <asgu23@gmail.com>
2017-01-30 23:00:07 +0300

    Initial Commit

---
+++ ToDo List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo List
+//
+//  Created by Jackson on 30/01/2017.
+//  Copyright © 2017 Jackson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
